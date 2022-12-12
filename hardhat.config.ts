bcede47ba8d4f10af98d7c1e2b460a09f7ba3f17
Linh <lixxkook@Macbut-M1-cua-Linh.local>
2022-12-12 14:11:34 +0700

    switch to ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,12 +1,13 @@
-require("@nomicfoundation/hardhat-toolbox");
-require("@nomiclabs/hardhat-etherscan");
-require("dotenv").config();
+import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
+dotenv.config({ path: __dirname + "/.env" });
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
+  solidity: "0.8.9",
   networks: {
     bsctest: {
-      url: `https://data-seed-prebsc-1-s1.binance.org:8545/`,
+      url: "https://data-seed-prebsc-1-s1.binance.org:8545",
       accounts: [process.env.PRIV_KEY]
     }
   },
